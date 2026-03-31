4a16156adeac803fcf20dc2f1645513c1c1136f0
George Mann <9221887+mannly01@users.noreply.github.com>
2026-03-31 14:40:37 -0700

    Add project files.

---
+++ DemoCheats/MelonLoaderMod.cs
@@ -0,0 +1,31 @@
+﻿using MelonLoader;
+
+namespace DemoCheats
+{
+    public static class BuildInfo
+    {
+        public const string Name = "DemoCheats";
+        public const string Author = "REPLACE_ME";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    public class DemoCheats : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("OnApplicationStart");
+        }
+
+        public override void OnSceneWasInitialized(int buildindex, string sceneName)
+        {
+            MelonLogger.Msg("OnSceneWasInitialized: " + buildindex.ToString() + " | " + sceneName);
+        }
+
+        public override void OnUpdate()
+        {
+            MelonLogger.Msg("OnUpdate");
+        }
+    }
+}
---
+++ DemoCheats/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(DemoCheats.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(DemoCheats.BuildInfo.Company)]
+[assembly: AssemblyProduct(DemoCheats.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + DemoCheats.BuildInfo.Author)]
+[assembly: AssemblyTrademark(DemoCheats.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(DemoCheats.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(DemoCheats.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(DemoCheats.DemoCheats), DemoCheats.BuildInfo.Name, DemoCheats.BuildInfo.Version, DemoCheats.BuildInfo.Author, DemoCheats.BuildInfo.DownloadLink)]
+[assembly: MelonGame(null, null)]
